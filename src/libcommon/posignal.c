7cf5ffda5cb128542b23a452ef8651544c78d52c
Chris Dunlap <cdunlap@llnl.gov>
2004-05-04 23:41:41 +0000

    Tweaked whitespace.

--- src/libcommon/posignal.c
+++ src/libcommon/posignal.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
- *  $Id: posignal.c,v 1.4 2004/04/30 19:28:35 dun Exp $
+ *  $Id: posignal.c,v 1.5 2004/05/04 23:41:41 dun Exp $
  *****************************************************************************
  *  This file is part of the Munge Uid 'N' Gid Emporium (MUNGE).
  *  For details, see <http://www.llnl.gov/linux/munge/>.
@@ -30,7 +30,6 @@
 #if HAVE_CONFIG_H
 #  include "config.h"
 #endif /* HAVE_CONFIG_H */
-
 
 #include <errno.h>
 #include <signal.h>
