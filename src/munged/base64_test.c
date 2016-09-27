4699a9f14449511e0d2de7b13ae41525d0e222a9
Chris Dunlap <cdunlap@llnl.gov>
2016-09-27 12:41:04 -0700

    Fix base64_test pointer-sign warnings
    When running "make check" with CFLAGS="-Wall -Werror", gcc 4.9.2 on
    Debian 8.6 generates 16 "pointer-sign" errors in base64_test.c in
    calls to strlen() and strncmp():
    
    > base64_test.c: In function 'validate':
    > base64_test.c:76:45: error: pointer targets in passing argument 1
    >       of 'strlen' differ in signedness [-Werror=pointer-sign]
    >      if (encode_block (buf, &n, src, strlen (src)) < 0)
    >                                              ^
    > In file included from base64_test.c:31:0:
    > /usr/include/string.h:399:15: note: expected 'const char *'
    >       but argument is of type 'const unsigned char *'
    >  extern size_t strlen (const char *__s)
    >                ^
    
    > base64_test.c:80:18: error: pointer targets in passing argument 1
    >       of 'strncmp' differ in signedness [-Werror=pointer-sign]
    >      if (strncmp (dst, buf, n))
    >                   ^
    > In file included from base64_test.c:31:0:
    > /usr/include/string.h:147:12: note: expected 'const char *'
    >       but argument is of type 'const unsigned char *'
    >  extern int strncmp (const char *__s1, const char *__s2, size_t __n)
    >             ^
    
    This commit revamps the test code to fix all compiler warnings.
    The unsigned type qualifier is removed from the dst arrays.
    The terminating null character is removed from the src arrays since
    they are not technically strings; their length is now computed via
    sizeof instead of strlen().  The src arrays are now passed as "void *";
    since encode_context() and decode_context() do pointer arithmetic with
    the src parm, this parm is cast to a local "const unsigned char *" var.
    Function prototypes are updated accordingly.
    
    Signed-off-by: Chris Dunlap <cdunlap@llnl.gov>

--- src/munged/base64_test.c
+++ src/munged/base64_test.c
@@ -36,31 +36,31 @@
  *    section 6.5 (Examples of Radix-64).
  */
 
-int validate       (const unsigned char *src, const unsigned char *dst);
-int encode_block   (unsigned char *dst, int *dstlen,
-                    const unsigned char *src, int srclen);
-int encode_context (unsigned char *dst, int *dstlen,
-                    const unsigned char *src, int srclen);
-int decode_block   (unsigned char *dst, int *dstlen,
-                    const unsigned char *src, int srclen);
-int decode_context (unsigned char *dst, int *dstlen,
-                    const unsigned char *src, int srclen);
+int validate (const char *dst, const void *src, int srclen);
+int encode_block (char *dst, int *dstlen, const void *src, int srclen);
+int encode_context (char *dst, int *dstlen, const void *src, int srclen);
+int decode_block (char *dst, int *dstlen, const void *src, int srclen);
+int decode_context (char *dst, int *dstlen, const void *src, int srclen);
 
 
 int
 main (int argc, char *argv[])
 {
-    const unsigned char src1[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9, 0x7e, 0x00 };
-    const unsigned char src2[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9, 0x00 };
-    const unsigned char src3[] = { 0x14, 0xfb, 0x9c, 0x03, 0x00 };
+    const unsigned char src1[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9, 0x7e };
+    const unsigned char src2[] = { 0x14, 0xfb, 0x9c, 0x03, 0xd9 };
+    const unsigned char src3[] = { 0x14, 0xfb, 0x9c, 0x03 };
 
-    const unsigned char dst1[] = "FPucA9l+";
-    const unsigned char dst2[] = "FPucA9k=";
-    const unsigned char dst3[] = "FPucAw==";
+    const char dst1[] = "FPucA9l+";
+    const char dst2[] = "FPucA9k=";
+    const char dst3[] = "FPucAw==";
 
-    if ( (validate (src1, dst1) < 0)
-      || (validate (src2, dst2) < 0)
-      || (validate (src3, dst3) < 0) ) {
+    if (validate (dst1, src1, sizeof (src1)) < 0) {
+        exit (EXIT_FAILURE);
+    }
+    if (validate (dst2, src2, sizeof (src2)) < 0) {
+        exit (EXIT_FAILURE);
+    }
+    if (validate (dst3, src3, sizeof (src3)) < 0) {
         exit (EXIT_FAILURE);
     }
     exit (EXIT_SUCCESS);
@@ -68,12 +68,12 @@ main (int argc, char *argv[])
 
 
 int
-validate (const unsigned char *src, const unsigned char *dst)
+validate (const char *dst, const void *src, int srclen)
 {
     int n;
-    unsigned char buf[9];
+    char buf[9];
 
-    if (encode_block (buf, &n, src, strlen (src)) < 0)
+    if (encode_block (buf, &n, src, srclen) < 0)
         return (-1);
     if (n != strlen (dst))
         return (-1);
@@ -82,12 +82,12 @@ validate (const unsigned char *src, const unsigned char *dst)
 
     if (decode_block (buf, &n, dst, strlen (dst)) < 0)
         return (-1);
-    if (n != strlen (src))
+    if (n != srclen)
         return (-1);
     if (strncmp (src, buf, n))
         return (-1);
 
-    if (encode_context (buf, &n, src, strlen (src)) < 0)
+    if (encode_context (buf, &n, src, srclen) < 0)
         return (-1);
     if (n != strlen (dst))
         return (-1);
@@ -96,7 +96,7 @@ validate (const unsigned char *src, const unsigned char *dst)
 
     if (decode_context (buf, &n, dst, strlen (dst)) < 0)
         return (-1);
-    if (n != strlen (src))
+    if (n != srclen)
         return (-1);
     if (strncmp (src, buf, n))
         return (-1);
@@ -106,26 +106,25 @@ validate (const unsigned char *src, const unsigned char *dst)
 
 
 int
-encode_block (unsigned char *dst, int *dstlen,
-              const unsigned char *src, int srclen)
+encode_block (char *dst, int *dstlen, const void *src, int srclen)
 {
     return (base64_encode_block (dst, dstlen, src, srclen));
 }
 
 
 int
-encode_context (unsigned char *dst, int *dstlen,
-                const unsigned char *src, int srclen)
+encode_context (char *dst, int *dstlen, const void *src, int srclen)
 {
     base64_ctx x;
     int i;
     int n;
     int m;
+    const unsigned char *p = src;
 
     if (base64_init (&x) < 0)
         return (-1);
-    for (i=0, n=0; i<srclen; i++) {
-        if (base64_encode_update (&x, dst, &m, src + i, 1) < 0)
+    for (i = 0, n = 0; i < srclen; i++) {
+        if (base64_encode_update (&x, dst, &m, p + i, 1) < 0)
             return (-1);
         dst += m;
         n += m;
@@ -141,26 +140,25 @@ encode_context (unsigned char *dst, int *dstlen,
 
 
 int
-decode_block (unsigned char *dst, int *dstlen,
-              const unsigned char *src, int srclen)
+decode_block (char *dst, int *dstlen, const void *src, int srclen)
 {
     return (base64_decode_block (dst, dstlen, src, srclen));
 }
 
 
 int
-decode_context (unsigned char *dst, int *dstlen,
-                const unsigned char *src, int srclen)
+decode_context (char *dst, int *dstlen, const void *src, int srclen)
 {
     base64_ctx x;
     int i;
     int n;
     int m;
+    const unsigned char *p = src;
 
     if (base64_init (&x) < 0)
         return (-1);
-    for (i=0, n=0; i<srclen; i++) {
-        if (base64_decode_update (&x, dst, &m, src + i, 1) < 0)
+    for (i = 0, n = 0; i < srclen; i++) {
+        if (base64_decode_update (&x, dst, &m, p + i, 1) < 0)
             return (-1);
         dst += m;
         n += m;
