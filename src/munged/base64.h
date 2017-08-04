9573b1659102fb080aa722df8ef6938f302dd13c
Albert Chu <chu11@llnl.gov>
2017-08-03 17:30:13 -0700

    Fix base64.h documentation
    base64_encode_block() and base64_decode_block() require dstlen
    to be passed in, but the header files say they are optional.
    Adjust documentation appropriately.
    
    Source:
    - https://github.com/flux-framework/flux-core/commit/e5bf5aafd61a973a5b8578b11cc55c1ce8ebea32
    
    Signed-off-by: Chris Dunlap <cdunlap@llnl.gov>

--- src/munged/base64.h
+++ src/munged/base64.h
@@ -106,15 +106,15 @@ int base64_cleanup (base64_ctx *x);
 
 int base64_encode_block (void *dst, int *dstlen, const void *src, int srclen);
 /*
- *  Base64-encodes [srclen] bytes from the contiguous [src] into [dst].
- *    If [dstlen] is not NULL, it will be set to the number of bytes written.
+ *  Base64-encodes [srclen] bytes from the contiguous [src] into [dst],
+ *    and sets [dstlen] to the number of bytes written.
  *  Returns 0 on success, or -1 on error.
  */
 
 int base64_decode_block (void *dst, int *dstlen, const void *src, int srclen);
 /*
- *  Base64-decodes [srclen] bytes from the contiguous [src] into [dst].
- *    If [dstlen] is not NULL, it will be set to the number of bytes written.
+ *  Base64-decodes [srclen] bytes from the contiguous [src] into [dst],
+ *    and sets [dstlen] to the number of bytes written.
  *  Returns 0 on success, or -1 on error.
  */
 
