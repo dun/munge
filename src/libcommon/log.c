dun/munge

commit feccf43495fbf867e98e943463779348885cdc32

Refactor daemonpipe abstraction
During "daemonization", the parent process blocks upon reading from a
pipe until signaled by a write from its child or grandchild process,
after which it exits.  Error messages from the child or grandchild
process can be written across this pipe in order to be relayed to
stderr by the parent process.  Management of this pipe is scattered
throughout daemonize_init(), daemonize_fini(), log_set_err_pipe(),
and _log_die().

Refactor the "daemonpipe" abstraction to make the code more robust,
understandable, and maintainable.

Tested:
- CentOS 7.6.1810, 6.10
- Debian sid, 9.8, 8.11, 7.11, 6.0.10, 5.0.10, 4.0, 3.1
- Fedora 29
- FreeBSD 11.2
- NetBSD 7.1.2
- OpenBSD 6.3
- Ubuntu 18.10, 18.04.2 LTS, 16.04.6 LTS, 14.04.6 LTS, 12.04.5 LTS

Signed-off-by: Chris Dunlap <cdunlap@llnl.gov>

src/libcommon/log.c
@@ -41,6 +41,7 @@
 #include <syslog.h>
 #include <time.h>
 #include <unistd.h>
+#include "daemonpipe.h"
 #include "log.h"
 #include "str.h"
 
@@ -65,7 +66,6 @@ struct log_ctx {
     int   got_syslog;
     int   priority;
     int   options;
-    int   fd_daemonize;
     char  id [LOG_IDENTITY_MAXLEN];
 };
 
@@ -74,7 +74,7 @@ struct log_ctx {
  *  Static Variables
  *****************************************************************************/
 
-static struct log_ctx log_ctx = { NULL, 0, 0, 0, 0, -1, { '\0' } };
+static struct log_ctx log_ctx = { NULL, 0, 0, 0, 0, { '\0' } };
 
 
 /*****************************************************************************
@@ -83,7 +83,7 @@ static struct log_ctx log_ctx = { NULL, 0, 0, 0, 0, -1, { '\0' } };
 
 static void _log_aux (int errnum, int priority, char *msgbuf, int msgbuflen,
         const char *format, va_list vargs);
-static void _log_die (int status, int priority, char *msg);
+static void _log_die (int status, int priority, const char *msg);
 static char * _log_prefix (int priority);
 
 
@@ -167,14 +167,6 @@ log_close_all (void)
 {
     log_close_file ();
     log_close_syslog ();
-    return;
-}
-
-
-void
-log_set_err_pipe (int fd)
-{
-    log_ctx.fd_daemonize = (fd >= 0) ? fd : -1;
     return;
 }
 
@@ -394,24 +386,14 @@ _log_aux (int errnum, int priority, char *msgbuf, int msgbuflen,
 
 
 static void
-_log_die (int status, int priority, char *msg)
+_log_die (int status, int priority, const char *msg)
 {
-    signed char  c;
-    int          n;
-    char        *p;
-
-    /*  Write error status and message to "daemonize" pipe.
+    /*  If the daemonpipe is open between the (grand)child process and the
+     *    parent process, relay the error message to the parent for output
+     *    onto stderr.
      */
-    if (log_ctx.fd_daemonize >= 0) {
-        c = (signed char) priority;
-        n = write (log_ctx.fd_daemonize, &c, sizeof (c));
-        if ((n > 0) && (msg != NULL) && (log_ctx.fp != stderr)) {
-            if ((p = strchr (msg, '\n'))) {
-                *p = '\0';
-            }
-            (void) write (log_ctx.fd_daemonize, msg, strlen (msg) + 1);
-        }
-    }
+    (void) daemonpipe_write (status, priority, msg);
+
 #ifndef NDEBUG
     /*  Generate core for debugging.
      */
@@ -419,6 +401,7 @@ _log_die (int status, int priority, char *msg)
         abort ();
     }
 #endif /* !NDEBUG */
+
     exit (status);
 }
 

src/munged/munged.c
@@ -52,6 +52,7 @@
 #include "common.h"
 #include "conf.h"
 #include "crypto.h"
+#include "daemonpipe.h"
 #include "gids.h"
 #include "hash.h"
 #include "job.h"
@@ -73,8 +74,8 @@
  *****************************************************************************/
 
 static void disable_core_dumps (void);
-static int daemonize_init (char *progname, conf_t conf);
-static void daemonize_fini (int fd);
+static void daemonize_init (char *progname, conf_t conf);
+static void daemonize_fini (void);
 static void open_logfile (const char *logfile, int priority, int got_force);
 static void handle_signals (void);
 static void sig_handler (int sig);
@@ -99,7 +100,6 @@ volatile sig_atomic_t got_terminate = 0;    /* signum if INT/TERM received   */
 int
 main (int argc, char *argv[])
 {
-    int   fd = -1;
     char *log_identity = argv[0];
     int   log_priority = LOG_INFO;
     int   log_options = LOG_OPT_PRIORITY;
@@ -117,7 +117,7 @@ main (int argc, char *argv[])
         conf->got_force);
 
     if (!conf->got_foreground) {
-        fd = daemonize_init (argv[0], conf);
+        daemonize_init (argv[0], conf);
         if (conf->got_syslog) {
             log_close_file ();
             log_open_syslog (log_identity, LOG_DAEMON);
@@ -148,7 +148,7 @@ main (int argc, char *argv[])
     write_pidfile (conf->pidfile_name, conf->got_force);
 
     if (!conf->got_foreground) {
-        daemonize_fini (fd);
+        daemonize_fini ();
     }
     log_msg (LOG_NOTICE, "Starting %s-%s daemon (pid %d)",
         PACKAGE, VERSION, (int) getpid ());
@@ -191,70 +191,61 @@ disable_core_dumps (void)
 }
 
 
-static int
+static void
 daemonize_init (char *progname, conf_t conf)
 {
 /*  Begins the daemonization of the process.
  *  Despite the fact that this routine backgrounds the process, control
  *    will not be returned to the shell until daemonize_fini() is called.
- *  Returns an 'fd' to pass to daemonize_fini() to complete the daemonization.
  */
-    int         fds [2];
-    pid_t       pid;
-    int         n;
-    signed char priority;
-    char        ebuf [1024];
+    pid_t pid;
+    int   status;
+    int   priority;
+    char  buf [1024];
 
     /*  Clear file mode creation mask.
      */
     umask (0);
 
-    /*  Create pipe for IPC so parent process will wait to terminate until
-     *    signaled by grandchild process.  This allows messages written to
-     *    stdout/stderr by the grandchild to be properly displayed before
-     *    the parent process returns control to the shell.
+    /*  Create a daemonpipe to have the parent process wait until signaled by
+     *    its double-forked grandchild process that startup is complete.
      */
-    if (pipe (fds) < 0) {
-        log_errno (EMUNGE_SNAFU, LOG_ERR, "Failed to create daemon pipe");
+    if (daemonpipe_create () < 0) {
+        log_errno (EMUNGE_SNAFU, LOG_ERR, "Failed to create daemonpipe");
     }
-    /*  Set the fd used by log_err() to return status back to the parent.
-     */
-    log_set_err_pipe (fds[1]);
-
     /*  Automatically background the process and
-     *    ensure child is not a process group leader.
+     *    ensure child process is not a process group leader.
      */
     if ((pid = fork ()) < 0) {
-        log_errno (EMUNGE_SNAFU, LOG_ERR, "Failed to create child process");
+        log_errno (EMUNGE_SNAFU, LOG_ERR, "Failed to fork child process");
     }
     else if (pid > 0) {
-        log_set_err_pipe (-1);
-        if (close (fds[1]) < 0) {
+        /*
+         *  Parent process waits for notification that startup is complete
+         *    before exiting.
+         */
+        if (daemonpipe_close_writes () < 0) {
             log_errno (EMUNGE_SNAFU, LOG_ERR,
-                "Failed to close write-pipe in parent process");
+                "Failed to close write-end of daemonpipe");
         }
-        if ((n = read (fds[0], &priority, sizeof (priority))) < 0) {
+        if (daemonpipe_read (&status, &priority, buf, sizeof (buf)) < 0) {
             log_errno (EMUNGE_SNAFU, LOG_ERR,
-                "Failed to read status from grandchild process");
+                "Failed to read from daemonpipe");
         }
-        if ((n > 0) && (priority >= 0)) {
-            if ((n = read (fds[0], ebuf, sizeof (ebuf))) < 0) {
-                log_errno (EMUNGE_SNAFU, LOG_ERR,
-                    "Failed to read err msg from grandchild process");
-            }
-            if ((n > 0) && (ebuf[0] != '\0')) {
-                log_open_file (stderr, progname, priority, LOG_OPT_PRIORITY);
-                log_msg (priority, "%s", ebuf);
-            }
+        if (status != 0) {
+            log_open_file (stderr, progname, priority, LOG_OPT_PRIORITY);
+            log_msg (priority, "%s", buf);
             exit (EXIT_FAILURE);
         }
         destroy_conf (conf, 0);
         log_close_all ();
         exit (EXIT_SUCCESS);
     }
-    if (close (fds[0]) < 0) {
+    /*  Child process continues.
+     */
+    if (daemonpipe_close_reads () < 0) {
         log_errno (EMUNGE_SNAFU, LOG_ERR,
-            "Failed to close read-pipe in child process");
+            "Failed to close read-end of daemonpipe");
     }
     /*  Become a session leader and process group leader
      *    with no controlling tty.
@@ -263,32 +254,32 @@ daemonize_init (char *progname, conf_t conf)
         log_errno (EMUNGE_SNAFU, LOG_ERR,
             "Failed to disassociate controlling tty");
     }
-    /*  Ignore SIGHUP to keep child from terminating when
-     *    the session leader (ie, the parent) terminates.
+    /*  Ignore SIGHUP to keep child process from terminating when
+     *    the session leader (i.e., the parent proces) terminates.
      */
     xsignal_ignore (SIGHUP);
 
-    /*  Abdicate session leader position in order to guarantee
-     *    daemon cannot automatically re-acquire a controlling tty.
+    /*  Abdicate session leader position to ensure the daemon cannot
+     *    automatically re-acquire a controlling tty.
      */
     if ((pid = fork ()) < 0) {
-        log_errno (EMUNGE_SNAFU, LOG_ERR,
-            "Failed to create grandchild process");
+        log_errno (EMUNGE_SNAFU, LOG_ERR, "Failed to fork grandchild process");
     }
     else if (pid > 0) {
         destroy_conf (conf, 0);
         log_close_all ();
         exit (EXIT_SUCCESS);
     }
-    return (fds[1]);
+    /*  Grandchild process continues.
+     */
+    return;
 }
 
 
 static void
-daemonize_fini (int fd)
+daemonize_fini (void)
 {
-/*  Completes the daemonization of the process,
- *    where 'fd' is the file descriptor returned by daemonize_init().
+/*  Completes the daemonization of the process.
  */
     int dev_null;
 
@@ -319,15 +310,18 @@ daemonize_fini (int fd)
     if ((dev_null > STDERR_FILENO) && (close (dev_null)) < 0) {
         log_errno (EMUNGE_SNAFU, LOG_ERR, "Failed to close \"/dev/null\"");
     }
-    /*  Clear the fd used by log_err() to return status back to the parent.
+    /*  Signal parent process to exit now that startup is complete.
+     *  The daemonpipe_write() below is not strictly necessary since
+     *    daemonpipe_close_writes() closes the daemonpipe which will cause
+     *    daemonpipe_read() to read an EOF.
      */
-    log_set_err_pipe (-1);
-    /*
-     *  Signal grandparent process to terminate.
-     */
-    if ((fd >= 0) && (close (fd) < 0)) {
+    if (daemonpipe_write (0, 0, NULL) < 0) {
         log_errno (EMUNGE_SNAFU, LOG_ERR,
-            "Failed to close write-pipe in grandchild process");
+            "Failed to signal parent process that startup is complete");
+    }
+    if (daemonpipe_close_writes () < 0) {
+        log_errno (EMUNGE_SNAFU, LOG_ERR,
+            "Failed to close write-end of daemonpipe");
     }
     return;
 }

src/libcommon/Makefile.am
@@ -39,6 +39,8 @@ libcommon_la_CPPFLAGS = \
 
 libcommon_la_SOURCES = \
 	common.h \
+	daemonpipe.c \
+	daemonpipe.h \
 	fd.c \
 	fd.h \
 	license.c \

src/libcommon/daemonpipe.c
@@ -1,0 +1,260 @@
+/*****************************************************************************
+ *  Written by Chris Dunlap <cdunlap@llnl.gov>.
+ *  Copyright (C) 2007-2019 Lawrence Livermore National Security, LLC.
+ *  Copyright (C) 2002-2007 The Regents of the University of California.
+ *  UCRL-CODE-155910.
+ *
+ *  This file is part of the MUNGE Uid 'N' Gid Emporium (MUNGE).
+ *  For details, see <https://dun.github.io/munge/>.
+ *
+ *  MUNGE is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.  Additionally for the MUNGE library (libmunge), you
+ *  can redistribute it and/or modify it under the terms of the GNU Lesser
+ *  General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  MUNGE is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  and GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with MUNGE.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "daemonpipe.h"
+#include "fd.h"
+
+
+/*****************************************************************************
+ *  Private Variables
+ *****************************************************************************/
+
+static int _daemonpipe_fd_read = -1;
+static int _daemonpipe_fd_write = -1;
+
+
+/*****************************************************************************
+ *  Public Functions
+ *****************************************************************************/
+
+/*  daemonpipe_create
+ *  Create a "daemonpipe" for IPC synchronization between the parent process
+ *    and its double-forked grandchild process during daemonization.
+ *  Return 0 on success, or -1 on error with errno set.
+ *  The parent process will block upon reading from this pipe until signaled by
+ *    a write from its child or grandchild process, after which it will exit.
+ *  The grandchild process will write to this pipe once startup is complete.
+ *  If startup fails, an error message will be written to the pipe by the child
+ *    or grandchild process in order for the parent process to relay it to
+ *    stderr before exiting.
+ */
+int
+daemonpipe_create (void)
+{
+    int fd_pipe [2];
+    int errno_bak;
+
+    if (pipe (fd_pipe) < 0) {
+        return (-1);
+    }
+    if (daemonpipe_close_reads () < 0) {
+        goto err;
+    }
+    if (daemonpipe_close_writes () < 0) {
+        goto err;
+    }
+    _daemonpipe_fd_read = fd_pipe[0];
+    _daemonpipe_fd_write = fd_pipe[1];
+    return (0);
+
+err:
+    errno_bak = errno;
+    (void) close (fd_pipe[0]);
+    (void) close (fd_pipe[1]);
+    errno = errno_bak;
+    return (-1);
+}
+
+
+/*  daemonpipe_close_reads
+ *  Close the read-end of the daemonpipe.
+ *  Return 0 on success, or -1 on error with errno set.
+ *  This should be called by the child process after having been forked.
+ */
+int
+daemonpipe_close_reads (void)
+{
+    if (_daemonpipe_fd_read < 0) {
+        return (0);
+    }
+    if (close (_daemonpipe_fd_read) < 0) {
+        return (-1);
+    }
+    _daemonpipe_fd_read = -1;
+    return (0);
+}
+
+
+/*  daemonpipe_close_writes
+ *  Close the write-end of the daemonpipe.
+ *  Return 0 on success, or -1 on error with errno set.
+ *  This should be called by the parent process after forking.
+ *  This should be called by the grandchild process once startup is complete.
+ *    It will signal the parent process blocked on daemonpipe_read().
+ */
+int
+daemonpipe_close_writes (void)
+{
+    if (_daemonpipe_fd_write < 0) {
+        return (0);
+    }
+    if (close (_daemonpipe_fd_write) < 0) {
+        return (-1);
+    }
+    _daemonpipe_fd_write = -1;
+    return (0);
+}
+
+
+/*  daemonpipe_read
+ *  Read a status code into [statusptr], a priority level into [priorityptr],
+ *    and an error string into the buffer [dstbufptr] of length [dstbuflen].
+ *    A status of 0 indicates success.
+ *  Return 0 on success, or -1 on error with errno set.
+ *  This should be called by the parent process once it is ready to block and
+ *    wait for its grandchild process to complete startup/initialization.
+ */
+int
+daemonpipe_read (int *statusptr, int *priorityptr,
+        char *dstbufptr, size_t dstbuflen)
+{
+    signed char c;
+    char        buf [1024];
+    ssize_t     n, m;
+
+    if ((statusptr == NULL) || (priorityptr == NULL) || (dstbufptr == NULL)) {
+        errno = EINVAL;
+        return (-1);
+    }
+    if (_daemonpipe_fd_read < 0) {
+        errno = EBADF;
+        return (-1);
+    }
+    /*  Initialize result parms in case of early return.
+     */
+    *statusptr = -1;
+    *priorityptr = 0;
+    if (dstbuflen > 0) {
+        dstbufptr[0] = '\0';
+    }
+    /*  Read status.
+     */
+    n = fd_read_n (_daemonpipe_fd_read, &c, sizeof (c));
+    if (n < 0) {
+        return (-1);
+    }
+    else if (n == 0) {                  /* if EOF, no err so return success */
+        *statusptr = 0;
+        return (0);
+    }
+    else if (n > 0) {
+        *statusptr = (int) c;
+    }
+    /*  Read priority.
+     */
+    n = fd_read_n (_daemonpipe_fd_read, &c, sizeof (c));
+    if (n < 0) {
+        return (-1);
+    }
+    else if (n == 0) {
+        return (0);
+    }
+    else if (n > 0) {
+        *priorityptr = (int) c;
+    }
+    /*  Read error message.
+     */
+    n = fd_read_n (_daemonpipe_fd_read, buf, sizeof (buf));
+    if (n < 0) {
+        return (-1);
+    }
+    else if ((n > 0) && (dstbuflen > 0)) {
+        /*
+         *  Ensure buf[] is null-terminated.
+         */
+        m = (n < sizeof (buf)) ? n : sizeof (buf) - 1;
+        buf[m] = '\0';
+        /*
+         *  Remove trailing LF if present.
+         */
+        m = strlen (buf) - 1;
+        if ((m >= 0) && (buf[m] == '\n')) {
+            buf[m] = '\0';
+        }
+        strncpy (dstbufptr, buf, dstbuflen);
+        dstbufptr[dstbuflen - 1] = '\0';
+    }
+    return (0);
+}
+
+
+/*  daemonpipe_write
+ *  Write a status code and an error message string [msg] at the specified
+ *    [priority] level to the daemonpipe.  A status of 0 indicates success.
+ *  Return 0 on success, or -1 on error with errno set.
+ *  This should be called by the child or grandchild process if an error
+ *    message needs to be relayed to the stderr of the parent process.
+ *    It will signal the parent process blocked on daemonpipe_read().
+ */
+int
+daemonpipe_write (int status, int priority, const char *msg)
+{
+    signed char c;
+    ssize_t     len;
+    ssize_t     n;
+
+    if (_daemonpipe_fd_write < 0) {
+        errno = EBADF;
+        return (-1);
+    }
+    /*  Write status.
+     */
+    c = (signed char) status;
+    len = sizeof (c);
+    n = fd_write_n (_daemonpipe_fd_write, &c, len);
+    if (n != len) {
+        return (-1);
+    }
+    /*  Write priority.
+     */
+    c = (signed char) priority;
+    len = sizeof (c);
+    n = fd_write_n (_daemonpipe_fd_write, &c, len);
+    if (n != len) {
+        return (-1);
+    }
+    /*  Write error message.  If no message is specified, write a null string.
+     */
+    if (msg == NULL) {
+        msg = "\0";
+    }
+    len = strlen (msg) + 1;
+    n = fd_write_n (_daemonpipe_fd_write, msg, len);
+    if (n != len) {
+        return (-1);
+    }
+    return (0);
+}

src/libcommon/daemonpipe.h
@@ -1,0 +1,52 @@
+/*****************************************************************************
+ *  Written by Chris Dunlap <cdunlap@llnl.gov>.
+ *  Copyright (C) 2007-2019 Lawrence Livermore National Security, LLC.
+ *  Copyright (C) 2002-2007 The Regents of the University of California.
+ *  UCRL-CODE-155910.
+ *
+ *  This file is part of the MUNGE Uid 'N' Gid Emporium (MUNGE).
+ *  For details, see <https://dun.github.io/munge/>.
+ *
+ *  MUNGE is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.  Additionally for the MUNGE library (libmunge), you
+ *  can redistribute it and/or modify it under the terms of the GNU Lesser
+ *  General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  MUNGE is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  and GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with MUNGE.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+
+#ifndef DAEMONPIPE_H
+#define DAEMONPIPE_H
+
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <sys/types.h>
+
+
+int daemonpipe_create (void);
+
+int daemonpipe_close_reads (void);
+
+int daemonpipe_close_writes (void);
+
+int daemonpipe_read (int *statusptr, int *priorityptr, char *dstbufptr,
+        size_t dstbuflen);
+
+int daemonpipe_write (int status, int priority, const char *msg);
+
+
+#endif /* !DAEMONPIPE_H */

src/libcommon/log.h
@@ -81,15 +81,6 @@ void log_close_all (void);
  *  Closes all logging devices that are open.
  */
 
-void log_set_err_pipe (int fd);
-/*
- *  Sets the file-descriptor for the write-half of the daemonize pipe
- *    connecting the original parent process to the forked grandchild process
- *    under which the daemon will continue running.
- *  If set (ie, fd >= 0), log_err() & log_errno() will return an error status
- *    and error message string back to the original parent process.
- */
-
 void log_err (int status, int priority, const char *format, ...);
 /*
  *  Logs a fatal message at the specified [priority] level according to

commit 89ef420e3dcbdad9019080999c92dc4efda2a77b

Fix dup err msg to stderr for failure to open logfile
When starting munged as root after having started it as a
non-privileged user, the following error message was written to
stderr twice:

> munged: Error: Logfile is insecure: "/var/log/munge/munged.log"
>   should be owned by UID 0 instead of UID 993
> munged: Error: Logfile is insecure: "/var/log/munge/munged.log"
>   should be owned by UID 0 instead of UID 993

When munged is started as a traditional daemon process (i.e., not
run in the foreground), log messages are initially written to stderr.
The process will double-fork() during daemonization and disassociate
from the tty.  Log messages will pivot to either the logfile or syslog.
If an error occurs before daemonization completes, the error message
will be relayed from the (grand)child process across the "daemonpipe"
to the parent process in order to be written to stderr.

In the error above, the first write was by the grandchild process
(i.e., after the double-fork() in daemonize_init()) before log
messages had pivoted to the logfile.  The grandchild process then
relayed the error message across the daemonpipe to have the parent
process write it to stderr before exiting.  This bug was introduced
in commit feccf43 when the daemonpipe abstraction was added.

Fix the duplicate message by having daemonpipe_write() only relay
the error message text if it has not already been written to stderr.

Tested:
- CentOS 7.6.1810, 6.10
- Debian sid, 10.0, 9.9, 8.11, 7.11, 6.0.10, 5.0.10, 4.0
- Fedora 30, 29
- FreeBSD 12.0-RELEASE-p9, 11.2-RELEASE-p13
- NetBSD 8.0, 7.1.2
- OpenBSD 6.5, 6.4
- openSUSE Leap 15.1
- Ubuntu 19.04, 18.04.3 LTS, 16.04.6 LTS, 14.04.6 LTS, 12.04.5 LTS

Signed-off-by: Chris Dunlap <cdunlap@llnl.gov>

src/libcommon/log.c
@@ -389,10 +389,12 @@ static void
 _log_die (int status, int priority, const char *msg)
 {
     /*  If the daemonpipe is open between the (grand)child process and the
-     *    parent process, relay the error message to the parent for output
-     *    onto stderr.
+     *    parent process, relay the error message to the parent for output onto
+     *    stderr.  But if the error message has already been written to stderr,
+     *    simply relay the status without the message text.
      */
-    (void) daemonpipe_write (status, priority, msg);
+    (void) daemonpipe_write (status, priority,
+            (log_ctx.fp != stderr) ? msg : NULL);
 
 #ifndef NDEBUG
     /*  Generate core for debugging.

src/munged/munged.c
@@ -233,8 +233,10 @@ daemonize_init (char *progname, conf_t conf)
                 "Failed to read from daemonpipe");
         }
         if (status != 0) {
-            log_open_file (stderr, progname, priority, LOG_OPT_PRIORITY);
-            log_msg (priority, "%s", buf);
+            if ((priority >= 0) && (buf[0] != '\0')) {
+                log_open_file (stderr, progname, priority, LOG_OPT_PRIORITY);
+                log_msg (priority, "%s", buf);
+            }
             exit (EXIT_FAILURE);
         }
         destroy_conf (conf, 0);

t/0103-munged-security-logfile.t
@@ -336,4 +336,19 @@ test_expect_success 'logfile dir writable by other with sticky bit' '
     munged_stop_daemon
 '
 
+# Check for a regression of a duplicate error message being written to stderr
+#   for a failure to open the logfile.
+##
+test_expect_success 'logfile failure writes single message to stderr' '
+    local ERR NUM &&
+    rm -f "${MUNGE_LOGFILE}" &&
+    touch "${MUNGE_LOGFILE}" &&
+    chmod 0400 "${MUNGE_LOGFILE}" &&
+    test_must_fail munged_start_daemon t-keep-logfile 2>err.$$ &&
+    cat err.$$ &&
+    ERR=$(sed -n -e "s/.*Error: //p" err.$$ | sort | uniq -c | sort -n -r) &&
+    NUM=$(echo "${ERR}" | awk "{ print \$1; exit }") &&
+    test "${NUM}" -eq 1 2>/dev/null
+'
+
 test_done