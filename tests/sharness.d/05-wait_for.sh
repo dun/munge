2ae1bd44ae762c5edf3e28f1f251a8c2139c4fc2
Chris Dunlap <cdunlap@llnl.gov>
2025-12-19 14:46:29 -0800

    tests: Improve wait_for function comment

--- tests/sharness.d/05-wait_for.sh
+++ tests/sharness.d/05-wait_for.sh
@@ -1,6 +1,6 @@
-# Wait for command $1 to succeed (rv 0) within timeout $2 seconds (default 5).
-#   The command is executed every $3 seconds (default 0.1) until it succeeds
-#   or the timeout is reached.
+# Wait for command $1 to succeed within $2 seconds (default 5).
+#   The command is executed every $3 seconds (default 0.1) until success
+#   (exit status 0) or the timeout is reached.
 # Return 0 if command succeeds within the timeout, 1 if the timeout expires.
 #
 wait_for()
